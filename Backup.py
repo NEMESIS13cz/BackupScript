6502e584018b9493816cad367309b689864466f7
Nemes <officialnemes@gmail.com>
2018-07-25 16:12:12 +0200

    Added support for borgbackup

--- Backup.py
+++ Backup.py
@@ -2,6 +2,7 @@ __author__ = "Nemes"
 
 import json
 import subprocess
+import datetime
 
 def convert(value):
     if value > (1024*1024*1024*5):
@@ -77,13 +78,22 @@ def is_available(source, destination, lockfile):
     out, err = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
     return str(out) == "b''"
 
-def run_backup(source, destination, directories, destination_dir):
-    dirlist = " --include ".join(directories)
-    cmd = ""
-    if destination is None:
-        cmd = "rdiff-backup --include " + dirlist + " --exclude / / " + destination_dir
+def run_backup(borg, snapshot_name, source, destination, directories, destination_dir):
+    if borg:
+        dirlist = " ".join(directories)
     else:
-        cmd = "rdiff-backup --include " + dirlist + " --exclude / / " + destination + "::" + destination_dir
+        dirlist = " --include ".join(directories)
+    cmd = ""
+    if borg:
+        if destination is None:
+            cmd = "borg create -C zlib,6 " + destination_dir + "::" + snapshot_name + " " + dirlist
+        else:
+            cmd = "borg create -C zlib,6 " + destination + ":" + destination_dir + "::" + snapshot_name + " " + dirlist
+    else:
+        if destination is None:
+            cmd = "rdiff-backup --include " + dirlist + " --exclude / / " + destination_dir
+        else:
+            cmd = "rdiff-backup --include " + dirlist + " --exclude / / " + destination + "::" + destination_dir
     if source is not None:
         cmd = "/usr/bin/ssh " + source + " '" + cmd + "'"
     print(cmd + "\n")
@@ -95,6 +105,7 @@ def backup_computer(comp):
     dest = comp["destination"]
     name = comp["source-name"]
     dst_name = comp["destination-name"]
+    borg = comp["use-borg"]
     remote_src = None
     remote_dst = None
     destination = None
@@ -105,7 +116,10 @@ def backup_computer(comp):
         destination = dest[dest.find(':')+1:len(dest)]
     else:
         destination = dest;
-
+    if borg:
+        print("    Using: borgbackup")
+    else:
+        print("    Using: rdiff-backup")
     print("    Source: " + (name if remote_src is None else ("ssh:" + name)))
     print("    Destination: " + (name if remote_dst is None else ("ssh:" + dst_name)) + " " + destination)
     src_online = is_online(None, remote_src)
@@ -129,7 +143,7 @@ def backup_computer(comp):
     print("    Destination Size:")
     diff = get_file_size(remote_src, remote_dst, destination)
     print("    Backup Command:\n")
-    run_backup(remote_src, remote_dst, comp["directories"], destination)
+    run_backup(borg, name, remote_src, remote_dst, comp["directories"], destination)
     print("\n    Destination Size:")
     dst_size = get_file_size(remote_src, remote_dst, destination);
     diff = dst_size - diff;
